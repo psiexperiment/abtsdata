442b1833d0f1be8240ce72efb1dca203f1ef7d5f
Brad Buran <buran@ohsu.edu>
2025-10-28 10:09:33 -0700

    Tweaks to summarize_tinnitus_2AFC

--- abtsdata/summarize_tinnitus_2AFC.py
+++ abtsdata/summarize_tinnitus_2AFC.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 from palettable.tableau import BlueRed_6
+import seaborn as sns
 
 from psidata.api import Recording
-from psidata.util import add_default_options, DatasetManager, process_files
+from psidata.manager import add_default_options, DatasetManager, process_files
 
 
 ttypes = ['NBN', 'SAM', 'silence']
@@ -12,6 +14,9 @@ palette = {t: c for t, c in zip(ttypes, BlueRed_6.mpl_colors)}
 
 expected_suffixes = [
     'performance.pdf',
+    'category-performance.csv',
+    'nbn-performance.csv',
+    'stats.json',
 ]
 
 
@@ -27,7 +32,25 @@ def process_file(filename, manager):
     axes['correct'].sharex(axes['tpm'])
 
     # Correct vs. incorrect by trial
-    tl = fh.trial_log.copy()
+    try:
+        tl = fh.trial_log.copy()
+    except pd.errors.EmptyDataError:
+        # No trials in file. Save empty figure and files so we know it was processed.
+        manager.save_fig(figure, 'performance.pdf')
+        manager.save_df(pd.DataFrame(), 'category-performance.csv')
+        manager.save_df(pd.DataFrame(), 'nbn-performance.csv')
+        try:
+            n_pellets = fh.event_log['event'].str.startswith('deliver_').sum()
+        except pd.errors.EmptyDataError:
+            n_pellets = 0
+        stats = {
+            'n_trials': 0,
+            'tpm_average': 0,
+            'n_pellets': n_pellets,
+        }
+        manager.save_dict(stats, 'stats.json')
+        return
+
     tl['score'] = tl['correct'].astype('f') + np.random.uniform(-0.1, 0.1, size=len(tl))
     m = tl['response'] == 'no_response'
     tl.loc[m, 'score'] = np.random.uniform(-0.1, 0.1, size=m.sum()) - 1
@@ -57,10 +80,11 @@ def process_file(filename, manager):
     axes['rt'].set_xlabel('Response Time (sec)\n(trial start to hopper)')
 
     # Plot category performance
-    mean_correct = fh.trial_log.groupby('trial_type')['correct'].mean()
+    mean_correct = fh.trial_log.groupby('trial_type')['correct'].agg(['mean', 'size', 'sum'])
     for i, (ttype, color) in enumerate(palette.items()):
-        y = mean_correct.loc[ttype]
-        axes['cat_perf'].bar(i, y, color=color)
+        if ttype in mean_correct:
+            y = mean_correct.loc[ttype, 'mean']
+            axes['cat_perf'].bar(i, y, color=color)
     axes['cat_perf'].xaxis.set_ticks([0, 1, 2])
     axes['cat_perf'].xaxis.set_ticklabels(ttypes)
     axes['cat_perf'].set_ylabel('Fraction correct')
@@ -69,17 +93,26 @@ def process_file(filename, manager):
 
     tl = fh.trial_log.copy()
     tl['nbn_frequency'] = tl['nbn_frequency'].apply(lambda x: round(x / 1e3, 1))
-    nbn_mean = tl.query('trial_type == "NBN"').groupby(['nbn_frequency'])['correct'].mean()
+    nbn_mean = tl.query('trial_type == "NBN"').groupby(['nbn_frequency'])['correct'].agg(['mean', 'size', 'sum'])
     x = range(len(nbn_mean))
-    axes['nbn_perf'].bar(x, nbn_mean, color=palette['NBN'])
+    axes['nbn_perf'].bar(x, nbn_mean['mean'], color=palette['NBN'])
     axes['nbn_perf'].xaxis.set_ticks(x)
     axes['nbn_perf'].xaxis.set_ticklabels(nbn_mean.index, rotation=90)
-    axes['nbn_perf'].set_xlabel('NBN frequency (kHz)') 
+    axes['nbn_perf'].set_xlabel('NBN frequency (kHz)')
     axes['nbn_perf'].set_ylabel('Fraction correct')
     axes['nbn_perf'].axis(ymin=0, ymax=1)
     axes['nbn_perf'].axhline(0.5, ls=':', color='k')
 
+    stats = {
+        'n_trials': len(tpm),
+        'tpm_average': tpm.mean(),
+        'n_pellets': fh.event_log['event'].str.startswith('deliver_').sum(),
+    }
+
     manager.save_fig(figure, 'performance.pdf')
+    manager.save_df(mean_correct, 'category-performance.csv')
+    manager.save_df(nbn_mean, 'nbn-performance.csv')
+    manager.save_dict(stats, 'stats.json')
 
 
 def main():
